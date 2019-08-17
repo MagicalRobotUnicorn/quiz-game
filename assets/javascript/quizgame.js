1c7dfef919aa72c9c427a5fb1863a5f7d0d19086
John Weber <johnhenryweber@gmail.com>
2019-08-17 02:48:26 -0700

    Raw Files

---
+++ assets/javascript/quizgame.js
@@ -0,0 +1,30 @@
+// quiz question object
+// quiz question contains:
+//  * GIPHY API Access Code: BtLHjwFVEd9ldzm3g7ETWmE8L67pdyzT
+//  * LOTR API Access Code: M3wx3CzB_JnO47DkAinm
+//    * https://the-one-api.herokuapp.com/v1
+//    * /book/5cf5805fb53e011a64671582
+//    * Authorization: Bearer your-api-key-123
+
+// other answers randomized?
+
+// Potter API
+// $2a$10$keI8PT9iiqnbBl/NUjDhZeDd9X1cVemSK.K5eOE7ZH0maFQH9dQ1K
+
+
+
+// All answers in an array
+// Correct answers given index
+// Other answers randomized onload
+
+// Array Generated through scraping wikipedia with nodejs and cheerio 
+
+
+
+
+function QuizQuestion (question, answer, category){
+  this.Question = question;
+  this.Answer = answer;
+  this.Category = category;
+}
+
