c1133964a4c59edb71f90af1956f5590c3711581
John Weber <johnhenryweber@gmail.com>
2019-08-20 18:15:27 -0700

    Fixed End Quiz AJAX

--- assets/javascript/quizgame.js
+++ assets/javascript/quizgame.js
@@ -317,7 +317,7 @@ function endQuiz() {
   $questionDisplay.text('Congrats!');
   $buttonDisplay.append('<div id="resultHeading">Congrats!!</div>');
   $buttonDisplay.append('<div id="prepare">Your score is ' + score + ' out of a possible 31.</div>')
-  var queryURL = 'https://api.giphy.com/v1/gifs/' + positiveMemes[0] + '?api_key=BtLHjwFVEd9ldzm3g7ETWmE8L67pdyzT";
+  var queryURL = 'https://api.giphy.com/v1/gifs/' + positiveMemes[0] + '?api_key=BtLHjwFVEd9ldzm3g7ETWmE8L67pdyzT';
 
   $.ajax({
     url: queryURL,
